86d1715af96b6a4bb5b0738a1a4aa8122d033b4b
ShabriSebastian <shabri.siregarr@gmail.com>
2024-08-07 11:08:56 +0700

    Menghapus file input.java

--- Bu_Pasha_Materi_Class/TesSiswa.java
+++ Bu_Pasha_Materi_Class/TesSiswa.java
@@ -4,21 +4,21 @@ package Bu_Pasha_Materi_Class;
 public class TesSiswa {
     public static void main(String[] args) {
         //Membuat Objek
-        Siswa dhian = new Siswa();
-        Siswa ochi = new Siswa();
-        dhian.id = 32847978;
-        dhian.nama = "Ardian Pratama";
-        dhian.ipk = 1.0;
-        ochi.id = 84320984;
-        ochi.nama = "Navalosky Allenzo";
-        ochi.ipk = 4.0;
+        Siswa dhianSigma = new Siswa();
+        Siswa ochiSkibidi = new Siswa();
+        dhianSigma.id = 32847978;
+        dhianSigma.nama = "Ardian Pratama";
+        dhianSigma.ipk = 1.0;
+        ochiSkibidi.id = 84320984;
+        ochiSkibidi.nama = "Navalovsky Allenzo";
+        ochiSkibidi.ipk = -4.0;
         System.out.println("== Data Ardhian ==");
-        System.out.println("ID: " + dhian.id);
-        System.out.println("Nama: " + dhian.nama);
-        System.out.println("IPK: " + dhian.ipk);
-        System.out.println("== Data Ochi ==");
-        System.out.println("ID: " + ochi.id);
-        System.out.println("Nama: " + ochi.nama);
-        System.out.println("IPK: " + ochi.ipk);
+        System.out.println("ID: " + dhianSigma.id);
+        System.out.println("Nama: " + dhianSigma.nama);
+        System.out.println("IPK: " + dhianSigma.ipk);
+        System.out.println("== Data ochiSkibidi ==");
+        System.out.println("ID: " + ochiSkibidi.id);
+        System.out.println("Nama: " + ochiSkibidi.nama);
+        System.out.println("IPK: " + ochiSkibidi.ipk);
     }
 }
