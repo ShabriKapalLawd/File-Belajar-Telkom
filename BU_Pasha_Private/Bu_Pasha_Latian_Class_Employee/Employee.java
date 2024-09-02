3965991e0013aeeafdb135658e8a98d125c25c51
ShabriSebastian <shabri.siregarr@gmail.com>
2024-09-02 11:36:36 +0700

    Memodifikasi Employee dengan if else condition

--- BU_Pasha_Private/Bu_Pasha_Latian_Class_Employee/Employee.java
+++ BU_Pasha_Private/Bu_Pasha_Latian_Class_Employee/Employee.java
@@ -2,6 +2,8 @@ package BU_Pasha_Private.Bu_Pasha_Latian_Class_Employee;
 public class Employee {
     private String nama;
     private double gaji;
+    private float rating;
+    private boolean status;
 
     public Employee(String nama, double gaji){
         this.nama = nama;
@@ -12,6 +14,7 @@ public class Employee {
     public Employee(){
         nama = "Unknown";
         gaji = 0;
+        status = false;
     }
 
     public void print(){
@@ -26,6 +29,12 @@ public class Employee {
     public double getSalary(){
         return gaji;
     }
+    public float getRating(){
+        return rating;
+    }
+    public boolean employeeFired(){
+        return status;
+    }
 
     //Setter
     public void setName(String nama){
@@ -33,5 +42,17 @@ public class Employee {
     }
     public void setSalary(double gaji){
         this.gaji = gaji;
+    }
+    public void setRating(float rating){
+        this.rating = rating;
+    }
+    public void fire(){
+        this.status = true;
+    }
+    public void cutSalary(){
+        this.gaji = gaji*0.9;
+    }
+    public void increaseSalary(){
+        this.gaji = gaji*1.2;
     }
 }
--- BU_Pasha_Private/Bu_Pasha_Latian_Class_Employee/driverEmployee.java
+++ BU_Pasha_Private/Bu_Pasha_Latian_Class_Employee/driverEmployee.java
@@ -2,11 +2,68 @@ package BU_Pasha_Private.Bu_Pasha_Latian_Class_Employee;
 public class driverEmployee {
     public static void main(String[] args) {
         Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        Employee e3 = new Employee();
 
         e1.setName("Yanto");
         e1.setSalary(100_000_000);
+        e2.setName("Audy");
+        e2.setSalary(90_000_000);
+        e3.setName("Shabri");
+        e3.setSalary(150_000_000);
 
-        System.out.println("Nama: " + e1.getName());
-        System.out.println("Gaji: " + e1.getSalary());
+        e1.setRating(2);
+        e2.setRating(1000);
+        e3.setRating(10);
+
+        
+        if ((e1.getRating() < 5) & (e1.getRating() > 3)){
+            e1.cutSalary();
+            System.out.println(e1.getName() + " Dipotong gajinya 10%");
+        }
+        else if (e1.getRating() < 3){
+            e1.fire();
+            System.out.println(e1.getName() + " Dipecat");
+        }
+        else if ((e1.getRating() >= 5) & (e1.getRating() < 8)){
+            System.out.println(e1.getName() + " Aman");
+        }
+        else{
+            e1.increaseSalary();
+            System.out.println(e1.getName() + " Gajinya meningkat 20%");
+        }
+        if ((e2.getRating() < 5) & (e2.getRating() > 3)){
+            e2.cutSalary();
+            System.out.println(e2.getName() + " Dipotong gajinya 10%");
+        }
+        else if (e2.getRating() < 3){
+            e2.fire();
+            System.out.println(e2.getName() + " Dipecat");
+        }
+        else if ((e2.getRating() >= 5) & (e2.getRating() < 8)){
+            System.out.println(e2.getName() + " Aman");
+        }
+        else{
+            e2.increaseSalary();
+            System.out.println(e2.getName() + " Gajinya meningkat 20%");
+        }
+        if ((e3.getRating() < 5) & (e3.getRating() > 3)){
+            e3.cutSalary();
+            System.out.println(e3.getName() + " Dipotong gajinya 10%");
+        }
+        else if (e3.getRating() < 3){
+            e3.fire();
+            System.out.println(e3.getName() + " Dipecat");
+        }
+        else if ((e3.getRating() >= 5) & (e3.getRating() < 8)){
+            System.out.println(e3.getName() + " Aman");
+        }
+        else{
+            e3.increaseSalary();
+            System.out.println(e3.getName() + " Gajinya meningkat 20%");
+        }
+
+    
+
     }
 }
